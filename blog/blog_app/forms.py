1ff3c93990b987b2238c21809cc8b1f8a41dbb84
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-11-06 01:17:16 +0530

    restrict who can make blog posts

--- blog/blog_app/forms.py
+++ blog/blog_app/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
 
         widgets ={
             'title':forms.TextInput(attrs={'class':'form-control'}),
-            'author': forms.Select(attrs={'class':'form-control'}),
+            'author': forms.TextInput(attrs={'class':'form-control', 'value':'', 'id':'user', 'type':'hidden'}),
             'body': forms.Textarea(attrs={'class':'form-control'})
         }
 

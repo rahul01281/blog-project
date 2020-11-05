dfb1532e94223bb2e9e80a516bf8e7c7260742c2
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-11-06 01:43:43 +0530

    add likes

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
 from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -14,6 +15,14 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        #this will grab the post from the Post table with primary key 'pk'
+        stuff = get_object_or_404(Post, id = self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context["total_likes"] = total_likes
+        return context
 
 class AddPostView(CreateView):
     model = Post
@@ -29,3 +38,10 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article_detail', args=[str(pk)]))
+
+

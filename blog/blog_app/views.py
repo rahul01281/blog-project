1eb673493aec0d97065da2f09f1b43b8eedc5b3d
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-11-06 02:08:05 +0530

    unlike blog button

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -21,7 +21,13 @@ class ArticleDetailView(DetailView):
         #this will grab the post from the Post table with primary key 'pk'
         stuff = get_object_or_404(Post, id = self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id = self.request.user.id).exists():
+            liked = True
+
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 class AddPostView(CreateView):
@@ -41,7 +47,14 @@ class DeletePostView(DeleteView):
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id = request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('article_detail', args=[str(pk)]))
 
 

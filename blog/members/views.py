487f3526ff56c7e9fbc8335f04c28e091d6b018a
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-11-06 08:56:11 +0530

    styled edit profile page

--- blog/members/views.py
+++ blog/members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 # Create your views here.
 
@@ -12,7 +12,7 @@ class UserSignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 

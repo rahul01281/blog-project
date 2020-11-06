3d13b440dfe5a9b8c2c1cad18f6c7bb8872a1e29
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-11-06 08:26:15 +0530

    added update profile page

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserSignUpView
+from .views import UserSignUpView, UserEditView
 
 urlpatterns = [
     path('register/', UserSignUpView.as_view(), name='signup'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile')
 ]
